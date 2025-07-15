7b25e670eeb2b973d2c17436459df987b2c1ccfd
vishwakarmasachin68 <vksachin86@gmail.com>
2025-07-15 15:11:44 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import ChallanForm from './components/ChallanForm';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ChallanForm from "./components/ChallanForm";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
